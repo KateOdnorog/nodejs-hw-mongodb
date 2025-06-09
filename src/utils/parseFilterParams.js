fac618f62db907deaff51fe77402e86a09deee6b
Kateryna Odnoroh <katya.odnorog@gmail.com>
2025-06-09 21:47:48 +0300

    donehomework4

---
+++ src/utils/parseFilterParams.js
@@ -0,0 +1,25 @@
+const parseContactType = (type) => {
+  const isString = typeof type === 'string';
+  if (!isString) return;
+  const isType = ['work', 'home', 'personal'].includes(type);
+
+  if (isType(type)) return type;
+};
+
+const parseBoolean = (value) => {
+  if (typeof value !== 'string') return;
+  if (value === 'true') return true;
+  if (value === 'false') return false;
+};
+
+export const parseFilterParams = (query) => {
+  const { contactType, isFavourite } = query;
+
+  const parseContactType = parseContactType(contactType);
+  const parsedIsFavourite = parseBoolean(isFavourite);
+
+  return {
+    contactType: parseContactType,
+    isFavourite: parsedIsFavourite,
+  };
+};
